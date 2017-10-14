660aec33bf20abad49502444ddb23eac09d114e6
Nazar Ponochevnyi <nazar.ponochevnyi@gmail.com>
2017-10-14 11:45:54 +0300

    Change type of graph
    Change type of graph. Dictionary to Array.

--- 2-dfs-graph/2-dfs-graph.py
+++ 2-dfs-graph/2-dfs-graph.py
@@ -4,11 +4,12 @@
 <!--@ Search in graph @--!>
 
 Comments:
-In the 2-dfs-graph.in file, enter the links between the graph nodes in the
-form: node1 space node2. In the last line enter the name of the nodes, the
-connection between which you want to check, in the form: node1 space node2.
-Run the 2-dfs-graph.py file. In the file 2-dfs-graph.out, you will find the
-number 1 if the connection is possible, else you will find the number 0.
+In the 2-dfs-graph.in file, in first line, enter count of nodes, in the next
+line, enter the name of the nodes, the connection between which you want to
+check, in the form: node1 space node2. In the next lines enter the links
+between the graph nodes in the form: node1 space node2. Run the 2-dfs-graph.py
+file. In the file 2-dfs-graph.out, you will find the number 1 if the connection
+is possible, else you will find the number 0.
 
 Author: Nazar Ponochevnyi
 '''
@@ -18,19 +19,18 @@ Author: Nazar Ponochevnyi
 file = open('2-dfs-graph.in', 'r')
 data = [d.strip() for d in file.readlines()]
 file.close()
-nodes = data[:-1]
-node1 = int(data[-1].split()[0]) - 1
-node2 = int(data[-1].split()[1]) - 1
+count_of_nodes = int(data[0])
+node1 = int(data[1].split()[0]) - 1
+node2 = int(data[1].split()[1]) - 1
+nodes = data[2:]
 
 
 # Functions
 def add_dep(n):
     n1 = int(n.split()[0]) - 1
     n2 = int(n.split()[1]) - 1
-    if isinstance(graph.get(n1), list): graph[n1].append(n2)
-    else: graph[n1] = [n2]
-    if isinstance(graph.get(n2), list): graph[n2].append(n1)
-    else: graph[n2] = [n1]
+    graph[n1].append(n2)
+    graph[n2].append(n1)
 
 def dfs(graph, node, node_state):
     if not node_state[node]:
@@ -41,10 +41,10 @@ def dfs(graph, node, node_state):
 
 
 # Basic processes
-graph = {}
+graph = [[] for _ in range(count_of_nodes)]
 for node in nodes: add_dep(node)
 
-node_state = dfs(graph, node1, [False for _ in range(len(graph.keys()))])
+node_state = dfs(graph, node1, [False for _ in range(count_of_nodes)])
 result = 1 if node_state[node2] else 0
 
 
